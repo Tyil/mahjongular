142547e7c61c9a8404e2a8b8c7d4a83ee5a47601
Patrick Spek <p.spek@tyil.nl>
2017-05-18 14:24:53 +0200

    Fix all warnings in gameslist

--- src/app/gameslist/gameslist.component.ts
+++ src/app/gameslist/gameslist.component.ts
@@ -7,19 +7,19 @@ import { GamesService } from '../services/games.service';
   styleUrls: ['./gameslist.component.css']
 })
 export class GameslistComponent implements OnInit {
-  constructor(private gameService:GamesService){}
-
   games = [
-    { id: 1, state: "open" },
-    { id: 2, state: "open" },
-    { id: 3, state: "started" }
+    { id: 1, state: 'open' },
+    { id: 2, state: 'open' },
+    { id: 3, state: 'started' }
   ];
+
+  constructor(private gameService: GamesService) {
+  }
 
   ngOnInit() {
   }
 
   handleClick(gameId) {
-    alert("Opening game " + gameId);
+    alert('Opening game ' + gameId);
   }
-
 }
